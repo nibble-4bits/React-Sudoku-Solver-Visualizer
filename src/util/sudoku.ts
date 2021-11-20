7fb3bd55e71e829af45e372361078ec22ed9e7f0
Luis De Anda <ldeandac16@gmail.com>
2021-11-20 12:30:57 -0600

    Create functions to check if a number is valid to be placed in a certain cell

--- src/util/sudoku.ts
+++ src/util/sudoku.ts
@@ -7,3 +7,61 @@ export function generateSudokuBoard(): number[][] {
 export function generateSudokuCellRefs<T>(): React.RefObject<T>[][] {
   return Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => createRef()));
 }
+
+export function isValidValue(
+  testValue: number,
+  row: number,
+  col: number,
+  sudoku: number[][]
+): boolean {
+  return (
+    isValidRowValue(testValue, row, sudoku) &&
+    isValidColValue(testValue, col, sudoku) &&
+    isValidBoxValue(testValue, row, col, sudoku)
+  );
+}
+
+export function isValidRowValue(testValue: number, row: number, sudoku: number[][]): boolean {
+  for (let x = 0; x < sudoku[row].length; x++) {
+    const currValue = sudoku[row][x];
+    if (testValue === currValue) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function isValidColValue(testValue: number, col: number, sudoku: number[][]): boolean {
+  for (let y = 0; y < sudoku.length; y++) {
+    const currValue = sudoku[y][col];
+    if (testValue === currValue) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function isValidBoxValue(
+  testValue: number,
+  row: number,
+  col: number,
+  sudoku: number[][]
+): boolean {
+  const rowStart = row - (row % 3);
+  const rowEnd = rowStart + 3;
+  const colStart = col - (col % 3);
+  const colEnd = colStart + 3;
+
+  for (let y = rowStart; y < rowEnd; y++) {
+    for (let x = colStart; x < colEnd; x++) {
+      const currValue = sudoku[y][x];
+      if (testValue === currValue) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
