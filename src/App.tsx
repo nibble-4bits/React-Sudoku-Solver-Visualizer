c1d66d750c4c2c58ac79b7db7a721f48be8c9f66
Luis De Anda <ldeandac16@gmail.com>
2021-11-20 12:43:48 -0600

    Display SudokuBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import SudokuBoard from './components/SudokuBoard';
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App(): JSX.Element {
-  return <div></div>;
+  return (
+    <AppWrapper>
+      <SudokuBoard />
+    </AppWrapper>
+  );
 }
 
 export default App;
