3d85b611fc86342bed0b072ec8a070552b2cd885
Luis De Anda <ldeandac16@gmail.com>
2021-11-21 14:05:27 -0600

    Create util function to find next empty cell

--- src/util/sudoku.ts
+++ src/util/sudoku.ts
@@ -121,3 +121,15 @@ export function isValidBoxValue(
 
   return true;
 }
+
+export function findNextEmptyCell(sudoku: number[][]): [number, number] | null {
+  for (let i = 0; i < sudoku.length; i++) {
+    for (let j = 0; j < sudoku[i].length; j++) {
+      if (!sudoku[i][j]) {
+        return [i, j];
+      }
+    }
+  }
+
+  return null;
+}
