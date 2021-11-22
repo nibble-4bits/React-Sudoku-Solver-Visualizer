34be03af3ac32ec71584e95e5bbc69260fd5342a
Luis De Anda <ldeandac16@gmail.com>
2021-11-21 19:04:45 -0600

    Create util function to shuffle an array
    Implements Fisher-Yates algorithm

--- src/util/util.ts
+++ src/util/util.ts
@@ -30,3 +30,17 @@ export function random(min: number, max: number): number {
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function shuffle<T>(arr: T[]): T[] {
+  const arrCopy = deepCopyArray(arr) as T[];
+
+  for (let i = arrCopy.length - 1; i > 0; i--) {
+    const j = random(0, i);
+
+    const temp = arrCopy[i];
+    arrCopy[i] = arrCopy[j];
+    arrCopy[j] = temp;
+  }
+
+  return arrCopy;
+}
