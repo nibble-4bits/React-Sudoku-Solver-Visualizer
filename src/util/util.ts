1d594032cab5b67865a8bd77d5ac3d52fc5e9749
Luis De Anda <ldeandac16@gmail.com>
2021-11-24 23:58:54 -0600

    Document util functions

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,9 @@
+/**
+ * Clamps a value between an upper and lower bound
+ * @param value The value to be clamped, if necessary
+ * @param min The minimum allowed value
+ * @param max The maximum allowed value
+ */
 export function clamp(value: number, min: number, max: number): number {
   if (value < min) return min;
 
@@ -6,6 +12,10 @@ export function clamp(value: number, min: number, max: number): number {
   return value;
 }
 
+/**
+ * Returns the deep copy of an array
+ * @param array The array to be deep copied
+ */
 export function deepCopyArray(array: unknown[]): unknown {
   return array.map((el) => {
     if (Array.isArray(el)) {
@@ -15,6 +25,10 @@ export function deepCopyArray(array: unknown[]): unknown {
   });
 }
 
+/**
+ * Pauses execution for a certain number of milliseconds
+ * @param ms Number of milliseconds to pause
+ */
 export async function sleep(ms: number): Promise<void> {
   if (ms === 0) return;
 
@@ -23,14 +37,23 @@ export async function sleep(ms: number): Promise<void> {
   });
 }
 
+/**
+ * Returns a random integer between a `min` and `max` value (inclusive)
+ * @param min The minimum value in the range
+ * @param max The maximum value in the range
+ */
 export function random(min: number, max: number): number {
-  if (min >= max) {
-    throw new Error('min param MUST be less than max');
+  if (min > max) {
+    throw new Error('min param MUST be less or equal to max');
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Shuffles an array using the Fisher-Yates algorithm
+ * @param arr The array to be shuffled
+ */
 export function shuffle<T>(arr: T[]): T[] {
   const arrCopy = deepCopyArray(arr) as T[];
 
